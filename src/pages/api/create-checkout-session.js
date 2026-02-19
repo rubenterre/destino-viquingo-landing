917af8a552b8defcd20145daa62db617bb5e94f2
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2026-02-19 11:59:38 +0100

    Update: Payment Stripe system

---
+++ src/pages/api/create-checkout-session.js
@@ -0,0 +1,76 @@
+// src/pages/api/create-checkout-session.js
+import Stripe from 'stripe';
+
+const stripe = new Stripe(import.meta.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-10-16',
+});
+
+export const prerender = false; // muy importante: este endpoint no se prerenderiza
+
+export async function POST({ request }) {
+  try {
+    // 1. Leer el carrito que mandas desde /checkout (localStorage → fetch)
+    const body = await request.json(); // { cart: { id: {id, name, price, quantity}, ... } }
+    const cart = body.cart || {};
+
+    const items = Object.values(cart);
+
+    if (!items.length) {
+      return new Response(JSON.stringify({ error: 'Carrito baleiro' }), {
+        status: 400,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+
+// 2. Convertir el carrito en line_items para Checkout
+const line_items = items.map((item) => ({
+  quantity: item.quantity,
+  price_data: {
+    currency: 'eur',
+    unit_amount: item.price,
+    product_data: {
+      name: item.name,
+    },
+  },
+}));
+
+
+
+    // 3. Crear la sesión de Stripe Checkout
+    const session = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      line_items,
+      success_url: `${import.meta.env.STRIPE_SUCCESS_URL}?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: import.meta.env.STRIPE_CANCEL_URL,
+
+      shipping_address_collection: {
+        allowed_countries: ['ES'], // o ['ES', 'PT'] si amplías
+      },
+
+       shipping_options: [
+    { shipping_rate: 'shr_1T2Um1QbEqODwiBXalOOQPxQ' }, //envío pagado
+    {shipping_rate: 'shr_1T2UpnQbEqODwiBXHuhd4qZx'} // envío recollida
+  ],
+      phone_number_collection: {
+        enabled: true,
+      },
+
+      // meta { project: 'DestinoViquingo' },
+    });
+
+    // 4. Devolver la URL de la sesión para que el frontend redirija
+    return new Response(JSON.stringify({ url: session.url }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('Stripe Checkout error:', error);
+    return new Response(
+      JSON.stringify({ error: 'Erro ao crear a sesión de pago.' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
+  }
+}
